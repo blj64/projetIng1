20fc68c73dd57a8563198acf0e2c6c3652741169
durandnico <durandnico@cy-tech.fr>
2023-05-16 14:55:23 +0200

    bdd + exemple

---
+++ bdd/init.sql
@@ -0,0 +1,66 @@
+-- Path: bdd/init.sql
+-- Date: 2020-04-28 14:00:00
+DROP DATABASE IF EXISTS IAPAU;
+CREATE DATABASE IAPAU;
+USE IAPAU;
+
+CREATE TABLE DataChallenge {
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    startDate date NOT NULL,
+    endDate date NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+};
+
+CREATE TABLE Group {
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    idDataC INT NOT NULL,
+    idLeader INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (idDataC) REFERENCES DataChallenge(id),
+    FOREIGN KEY (idLeader) REFERENCES User(id)
+};
+
+CREATE TABLE User {
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(20) NOT NULL,
+    lastName VARCHAR(31) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    number VARCHAR(11) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+};
+
+CREATE TABLE Admin {
+    idUser INT NOT NULL,
+    PRIMARY KEY (idUser),
+    FOREIGN KEY (idUser) REFERENCES User(id)
+};
+
+CREATE TABLE Student {
+    idUser INT NOT NULL,
+    idGroup INT NOT NULL,
+    PRIMARY KEY (idUser),
+    FOREIGN KEY (idUser) REFERENCES User(id),
+    FOREIGN KEY (idGroup) REFERENCES Group(id)
+};
+
+CREATE TABLE Gestionnaire {
+    idUser INT NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    startDate date,
+    endDate date,
+    PRIMARY KEY (idUser),
+    FOREIGN KEY (idUser) REFERENCES User(id)
+};
+
+CREATE TABLE Gerer {
+    idUser INT NOT NULL,
+    idDataC INT NOT NULL,
+    PRIMARY KEY (idUser, idDataC),
+    FOREIGN KEY (idUser) REFERENCES User(id),
+    FOREIGN KEY (idDataC) REFERENCES DataChallenge(id)
+};
+
---
+++ bdd/init_data.sql
@@ -0,0 +1,24 @@
+-- Path: bdd/init_data.sql
+-- Date: 2020-04-28 15:00:00
+
+USE IAPAU;
+
+INSERT INTO User VALUES (null, "Nicolas", "Durand", "123456", "0606060606", "exemple@cy-tech.fr");
+INSERT INTO User VALUES (null, "Matt", "Coste", "123456", "0606060606", "exemple2@cy-tech.fr");
+INSERT INTO User VALUES (null, "admin", "admin", "123456", "0606060606", "admin@cy-tech.fr");
+INSERT INTO User VALUES (null, "Lilan", "gestionnaire", "123456", "0606060606", "gestion@cy-tech.fr");
+INSERT INTO User VALUES (null, "Lucas", "Fernandes", "123456", "0606060606", "exemple3@cy-tech.fr");
+
+INSERT INTO Admin VALUES (3);
+
+INSERT INTO Gestionnaire VALUES (4, "Cy-Tech", "2020-04-28", "2020-05-28");
+
+INSERT INTO DataChallenge VALUES (null, "DataChallenge1", "2020-04-28", "2020-05-28", "image1");
+
+INSERT INTO Group VALUES (null, "Group1", 1, 1);
+
+INSERT INTO Student VALUES (1, 1);
+INSERT INTO Student VALUES (2, 1);
+INSERT INTO Student VALUES (5, 1);
+
+INSERT INTO Gerer VALUES (4, 1);
