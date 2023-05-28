f69684dab8f5bb617b7efe3282b7a97cab1571f0
durandnico <nicod4956@gmail.com>
2023-05-28 03:25:21 +0200

    password w/ only number are broken :(

--- bdd/init_data.sql
+++ bdd/init_data.sql
@@ -4,10 +4,10 @@
 USE IAPau;
 
 INSERT INTO `User` VALUES (null, "Nicolas", "Durand", "$2y$10$zq6xyV8q0PWp9plNdQpewuZGIXCj8KJlyYYVk7wIu32ws0Lhjm1yK", "0606060606", "durandnico@cy-tech.fr"); /* Mot de passe : Azerty1! */
-INSERT INTO `User` VALUES (null, "Matt", "Coste", "$2y$10$Zjr2Wpmr6NkiXSIEIanYguGTgcFmmImDfRqvii/lCNEX914xiGhWK", "0606060606", "Costematt@cy-tech.fr"); /* Mot de passe : 123456 */
-INSERT INTO `User` VALUES (null, "admin", "admin", "$2y$10$1dC12ArtdzG2VHwqjvgSHuECfMDJTDR1.74KwfrlteIIGukzRCMxy", "0606060606", "admin@cy-tech.fr"); /* Mot de passe : admin */
+INSERT INTO `User` VALUES (null, "Matt", "Coste", "$2y$10$0nd2hf4zQ1AnAQy4dGaQ6eRJhr6Hy8Dh/8f4xlpICdsXS4DL74Dk2", "0606060606", "Costematt@cy-tech.fr"); /* Mot de passe : azerty */
+INSERT INTO `User` VALUES (null, "admin", "admin", "$2y$10$GqbX4WeZ9fG7wteXNSu1mOY6paNQms3xecowH8e1olfsrwA7RQvnG", "0606060606", "admin@cy-tech.fr"); /* Mot de passe : admin */
 INSERT INTO `User` VALUES (null, "Lilan", "manager", "$2y$10$QJAKD69.horLpTlHGU64Oemt6NeG8L2qODpZtkRL63kkKbB1H65Ge", "0606060606", "manager@cy-tech.fr"); /* Mot de passe : manager */
-INSERT INTO `User` VALUES (null, "Lucas", "Fernandes", "$2y$10$Zjr2Wpmr6NkiXSIEIanYguGTgcFmmImDfRqvii/lCNEX914xiGhWK", "0606060606", "fernandes@cy-tech.fr"); /* Mot de passe : 123456 */
+INSERT INTO `User` VALUES (null, "Lucas", "Fernandes", "$2y$10$0nd2hf4zQ1AnAQy4dGaQ6eRJhr6Hy8Dh/8f4xlpICdsXS4DL74Dk2", "0606060606", "fernandes@cy-tech.fr"); /* Mot de passe : azerty */
 
 INSERT INTO `Admin` VALUES (3);
 
