ca1d6ed2277b6c1115e86ee726d8d1602c7898df
durandnico <nicod4956@gmail.com>
2023-05-30 20:56:52 +0200

    debut de lesTeams

---
+++ pages/lesTeam.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/allTeam.css">
+    <link rel="stylesheet" href="/css/gestionUser.css">
+    <title>Les équipes</title>
+</head>
+<body>
+
+    <?php require_once ($_SERVER['DOCUMENT_ROOT'].'/php/header.php'); ?>
+    
+    <div class="main">
+        
+        <div class="center">
+
+            <div class="dataC">
+                <h1>
+                    NOM Du Data Challenge
+                </h1>
+
+                    <div class="event-img">
+                        <!--<img src="/asset/img/bigbrains.jpg" alt="image-event">
+                    -->
+                </div>             
+            </div>
+
+            <div class="team">
+                <p><span>NOM de la team</span></p>
+                <p>nb de membre : 5</p>
+                <p>Rendu : ?</p>
+                <a href="au pays des merveilles"><img src="/asset/icon/crown.ico" alt=""></a>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
