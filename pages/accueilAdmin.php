399c5126a41b05238db4ee8aa7ecb9e90a2ac2c8
LF2Zetrei <fernandesl@cy-tech.fr>
2023-05-31 14:50:20 +0200

    redirections

--- pages/accueilAdmin.php
+++ pages/accueilAdmin.php
@@ -58,7 +58,7 @@ session_start();
                     </div>
                 </a>');
                 }else if(roleUser($_SESSION['user']['id'], ADMIN)==1){
-                    echo('<a href="/pages/myGroup.php" id="Team" class="card">
+                    echo('<a href="/pages/gestionUser.php" id="Team" class="card">
                     <div class="filter">
                         <div class="card-img">
                             <h1>Gestion des gestionnaires</h1>
@@ -67,7 +67,7 @@ session_start();
                 </a>
 
 
-                <a href="/pages/dataC.php" id="DataC" class="card">
+                <a href="/pages/createChallenge.php" id="DataC" class="card">
                     <div class="filter">
                             <div class="card-img">
                             <h1>Gestion des challenges</h1>
@@ -109,6 +109,8 @@ session_start();
                         </div>
                     </div>
                 </a>');
+                }else{
+                    header('Location: /pages/index.php');
                 }
                 ?>
             </div>
--- pages/myGroup.php
+++ pages/myGroup.php
@@ -22,7 +22,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/header.css">
     <link rel="stylesheet" href="/css/myGroup.css">
+    <link rel="stylesheet" href="../css/footer.css">
     <title>IAPau my group</title>
     <style>
         canvas{
@@ -39,12 +41,10 @@
     </style>
 </head>
 
-<body id=body>
-    <?php //require_once $_SERVER["DOCUMENT_ROOT"] . '/php/header.php'; 
+<body>
+    <?php require_once $_SERVER["DOCUMENT_ROOT"] . '/php/header.php'; 
     ?>
-    <div style="height: 10vh; background-color:blueviolet;">
-        <h1>I'm a header</h1>
-    </div>
+    
     <div class="main">
 
         <div class="center">
@@ -223,10 +223,8 @@
 
     </div>
 
-    <div style="height: 10vh; background-color:red;padding-top:0px;">
-        <h1>I'm a footer</h1>
-    </div>
-    <?php //require_once $_SERVER["DOCUMENT_ROOT"] . '/php/footer.php'; 
+    
+    <?php require_once $_SERVER["DOCUMENT_ROOT"] . '/php/footer.php'; 
     ?>
 </body>
 <script src="/js/myGroup.js"></script>
