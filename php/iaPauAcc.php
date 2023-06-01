627e96e6b2ac74f43df80816195833457f43a5dd
Sky <costematt@cy-tech.fr>
2023-06-01 21:59:23 +0200

    Quelques modifications sur iaPauAcc.php (ajout des liens pour les réseaux)

--- php/iaPauAcc.php
+++ php/iaPauAcc.php
@@ -2,8 +2,8 @@
     <div id="ZIAP1">
         <div id="ZIAP1_left">
             <h1>IA Pau, c'est quoi ? </h1>
-            <p>L’association IA PAU rassemble dans la bonne humeur chercheurs, enseignants, étudiants, entrepreneurs, particuliers autour de la thématique du traitement des données et propose de rapprocher la sphère économique, le monde académique et le grand public en organisant des événements et des projets collaboratifs.
-Son objectif est de vulgariser et partager les connaissances autour de ce progrès majeur du XXIème siècle qui suscite de nombreuses interrogations scientifiques, technologiques, éthiques, et démocratiques.
+            <p>L’association IA PAU rassemble dans la bonne humeur, chercheurs, enseignants, étudiants, entrepreneurs, particuliers autour de la thématique du traitement des données et propose de rapprocher la sphère économique, le monde académique et le grand public en organisant des événements et des projets collaboratifs.
+Son objectif est de vulgariser et partager les connaissances autour de ce progrès majeur du XXIᵉ siècle qui suscite de nombreuses interrogations scientifiques, technologiques, éthiques, et démocratiques.
             </p>
         </div>
         <div id="ZIAP1_right">
@@ -11,8 +11,8 @@ Son objectif est de vulgariser et partager les connaissances autour de ce progr�
         </div>
     </div>
     <div id="ZIAP2">
-        <a href="" class="spaceBetweenLinkIAP"> linkedIn </a>
-        <a href="" class="spaceBetweenLinkIAP"> instagram </a>
-        <a href=""> facebook </a>
+        <a href="https://fr.linkedin.com/company/ia-pau" target="_blank" class="spaceBetweenLinkIAP"> linkedIn </a>
+        <a href="https://www.instagram.com/iapauassociation" target="_blank" class="spaceBetweenLinkIAP"> instagram </a>
+        <a href="https://www.facebook.com/IA-Pau-1494793403982576/?__xts__[0]=68.ARDOq4I4L0WvUT0kqdblc1P_L2uITLG-XGleZvuM_wIs6FuGYU3rcmhGZlsgo6dvZORji6E_WPOfEvA8YvvTw8Cy6e8yAbqbA2PbyW380s-J4LRXrurT7GaSpWbl9ByP0-XauAjmN_Y3XQDxOwSuE3pc2DNTJDtAAsozxUl1-NjHn1Mwz25dMA" target="_blank"> facebook </a>
     </div>
 </div>
